e1232c7b90a925b46f5f426f647731b03010143f
Koko est <61022938+MatheusAznavour@users.noreply.github.com>
2021-10-02 14:04:14 -0300

    incomplety v0.0.2
    Changed my <textarea> to a div, i implemented a Javascript function that helps a lot to format text and them not needing some functions.

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,40 +3,60 @@
 <!-- Bold -->
 <!-- Italic -->
 <!-- subscrip -->
-<!-- add a line -->
+<!-- insert a horizontal line -->
 <!-- Underline -->
 <!-- link -->
 */
+let textArea = document.querySelector('.textarea')
+const text = `In publishing and graphic design\n
+Lorem ipsum is a placeholder
+text commonly used to demonstrate the visual form of a document
+or a typeface without relying on meaningful content. Lorem ipsum may
+be used as a placeholder before final copy is available.`
+
+textArea.innerText = text
+
 
 const toolbox = document.querySelectorAll('button')
-let textArea = document.querySelector('textarea')
 
-var text = ''
 
-function getSelectedText(){
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-        console.log(text)
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    return text;
-}
-
+let clickedTimes = 0
 function formatString(){
     const self = {
         H1: () => {
-            let textAreaText = textArea.value.includes(text)
-            getSelectedText()
-
-            textAreaa.value = textArea.value.replace(text.toLowerCase(), text.toUpperCase())
-            console.log('SSS')
-            console.log(text)
-        }
+            if(clickedTimes < 1){
+                document.execCommand('fontSize', true,'5pt')
+                clickedTimes += 1
+            } else {
+                document.execCommand('fontSize', true,'3pt')
+                clickedTimes = 0
+            }
+        },
+        Bold: () => { document.execCommand('bold') },
+        Italic: () => {document.execCommand('italic')  },
+        Subscript: () => { document.execCommand('subscript') },
+        horizontalLine: () => { document.execCommand('insertHTML', true, '<hr/>') },
+        Underline: () => { document.execCommand('underline') }
     }
     return self
 }
 
 toolbox[0].addEventListener('click', ()=>{
     formatString().H1()
+})
+toolbox[1].addEventListener('click', ()=>{
+    formatString().Bold()
+})
+toolbox[2].addEventListener('click', ()=>{
+    formatString().Italic()
+})
+toolbox[3].addEventListener('click', ()=>{
+    formatString().Subscript()
+})
+toolbox[4].addEventListener('click', ()=>{
+    formatString().horizontalLine()
+    console.log('aa')
+})
+toolbox[5].addEventListener('click', ()=>{
+    formatString().Underline()
 })
